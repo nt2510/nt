fedaa66b446b7028c08bbe926f056c73fdb9e1d3
ntlee <lizheng2510@gmail.com>
2017-02-14 14:59:13 +0800

    C初試

---
+++ algorithm/C算法/C算法/first.c
@@ -0,0 +1,15 @@
+//
+//  first.c
+//  C算法
+//
+//  Created by ntlee on 12/02/2017.
+//  Copyright © 2017 ntlee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main2()
+{
+    //printf("Hello world");
+    return 0;
+}
