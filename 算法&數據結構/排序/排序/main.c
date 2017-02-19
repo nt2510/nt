c05d576b338e9b8ba9e98e9d0cdcae29622df3f0
ntlee <lizheng2510@gmail.com>
2017-02-19 17:46:53 +0800

    冒溝排序

--- 算法&數據結構/排序/排序/main.c
+++ 算法&數據結構/排序/排序/main.c
@@ -7,9 +7,33 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+typedef int Item;
+
+#define key(A) (A)
+#define less(A,B) (key(A) < key(B))
+#define exac(A,B) { Item t=A;A=B;B=t; }
+#define compexch(A,B) if(less(B,A)) exac(A,B);
+
+/*冒泡排序*/
+void BubbleSort(Item a[],int left, int right)
+{
+    int i,j;
+    for(i=left;i<right;i++)
+        for(j=right;j>i;j--)
+            compexch(a[j-1],a[j]);
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    
+    int a[5] = {3,5,4,1,2};
+    BubbleSort(a, 0, 4);
+    
+    
+    for(int i=0;i<5;i++)
+        printf("%3d",a[i]);
+    
+    printf("\n");
     return 0;
 }
