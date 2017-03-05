3d7e6adc2e0f8bee6075754166c6221ee5bb579d
ntlee <lizheng2510@gmail.com>
2017-03-05 17:05:51 +0800

    單鏈表 創建

--- 算法&數據結構/數據結構/數據結構/main.c
+++ 算法&數據結構/數據結構/數據結構/main.c
@@ -7,9 +7,50 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    create();
     return 0;
 }
+
+typedef struct student
+{
+    int data;
+    struct student *next;
+} node;
+
+node *create()
+{
+    node *head,*p,*n;
+    int x,cycle=1;
+    head = (node *)malloc(sizeof(node));
+    p = head;
+    while(cycle){
+        printf("\nplease input the data");
+        scanf("\d",&x);
+        if(x!=0){
+            s = (node *)malloc(sizeof(node));
+            s->data = x;
+            printf("\n%d",s->data);
+            p->next = s;
+            p = s;
+        }
+        else cycle = 0;
+        
+    }
+    head = head->next;
+    p->next = null;
+    printf("\n yyy %d",head->data);
+    return (head);
+}
+
+
+
+
