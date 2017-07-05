872042fc9b4a4d37c805d4804c28a52b03f2bb1c
ntlee <lizheng2510@gmail.com>
2017-07-05 18:49:17 +0800

    微信公众号

--- bbs/config/routers.php
+++ bbs/config/routers.php
@@ -7,6 +7,6 @@ Macaw::get('mongo', '\App\Home\MongoController@index');
 Macaw::get('job', '\App\Home\JobController@index');
 Macaw::get('comment', '\App\Home\CommentController@post');
 Macaw::get('wx', '\App\Home\WxController@index');
-
+Macaw::post('wx', '\App\Home\WxController@index');
 
 Macaw::dispatch();
